e5ba795b6bef553935c63624f86226f9dc646796
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2020-08-31 16:34:54 +0200

    refine placement of db message

--- create.sh
+++ create.sh
@@ -4,4 +4,4 @@ docker build -t haskell-template-setup .
 docker create --name haskell-template-setup haskell-template-setup
 docker cp haskell-template-setup:$ROOT $PWD/root
 docker rm -f haskell-template-setup
-echo copied $ROOT to $PWD/root
+echo copied $ROOT to $PWD/root/$(basename $ROOT)
