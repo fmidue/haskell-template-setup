8245d07e638227cc1ea54bfb177db97035a53b86
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2020-09-02 20:07:31 +0200

    enable changing stack image to use

--- create.sh
+++ create.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
-RESOLVER=$(sed -n 's/resolver: \(.*\)/\1/p' stack.yaml)
-echo using resolver: ${RESOLVER}
 source env
-docker build --build-arg TAG=${RESOLVER} -t haskell-template-setup .
+if ! [[ -v RESOLVER ]]
+then
+  IMAGE_TAG=$(sed -n 's/resolver: \(.*\)/\1/p' stack.yaml)
+fi
+echo using stack image with tag ${IMAGE_TAG}
+docker build --build-arg TAG=${IMAGE_TAG} -t haskell-template-setup .
 docker create --name haskell-template-setup haskell-template-setup
+rm -rf root
 docker cp haskell-template-setup:$ROOT $PWD/root
 docker rm -f haskell-template-setup
-echo copied $ROOT to $PWD/root/$(basename $ROOT)
+echo copied $ROOT to $PWD/root
