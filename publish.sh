0d7f5761fc455822a02ea991f0df35362032a29a
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2023-06-12 19:57:38 +0200

    add jump host option

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ then
   TARGET=${FOLDER}${TIME}
 fi
 PROGRESS=$(rsync --version | sed -n "1s/^.*version 3.[1-9].*$/--info=progress2/g;1p")
-rsync -a ${PROGRESS} --rsh="ssh -p ${PORT}" ${PWD}/root/ ${SSH_USER}@${SERVER}:${TARGET}/ \
-    && ssh -p ${PORT} ${SSH_USER}@${SERVER} "chown -R ${USER}:${GROUP} ${TARGET}\
+rsync -a ${PROGRESS} --rsh="ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+\"${JUMP_HOST}\"} -p ${PORT}" ${PWD}/root/ ${SSH_USER}@${SERVER}:${TARGET}/ \
+    && ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"} -p ${PORT} ${SSH_USER}@${SERVER} "chown -R ${USER}:${GROUP} ${TARGET}\
  && rm ${ROOT}\
  && ln -s ${FOLDER}${TIME} ${ROOT}"
